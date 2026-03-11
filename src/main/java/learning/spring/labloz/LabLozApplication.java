612436773ee240f7ef7f19336915c3ca271d6fd4
Dmitryi <dmitry@yandex.ru>
2026-03-11 20:15:20 +0300

    Project initialazing

---
+++ src/main/java/learning/spring/labloz/LabLozApplication.java
@@ -0,0 +1,13 @@
+package learning.spring.labloz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabLozApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabLozApplication.class, args);
+    }
+
+}
